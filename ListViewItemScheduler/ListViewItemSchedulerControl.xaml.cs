35d37305d88c7e4adbe5bcdd36772bfb311d0a04
YGenon <genonster@gmail.com>
2020-04-03 23:59:28 +0300

    создан проект с UserControl - ListViewItemScheduler

---
+++ ListViewItemScheduler/ListViewItemSchedulerControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace ListViewItemScheduler
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class ListViewItemSchedulerControl : UserControl
+    {
+        public ListViewItemSchedulerControl() => InitializeComponent();
+
+    }
+}
